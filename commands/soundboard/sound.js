02a4c26ffbaddf605822c6f96127eb2e8ebc21f8
wleung316957 <wleung316597x@gmail.com>
2017-12-14 17:05:46 -0800

    Add files via upload

--- commands/soundboard/sound.js
+++ commands/soundboard/sound.js
@@ -1,47 +1,39 @@
 const commando = require('discord.js-commando');
 
+
 class SoundCommand extends commando.Command {
     constructor(client) {
         super(client, {
             name: 'sound',
             group: 'soundboard',
             memberName: 'sound',
-            description: 'Collection of soundboards, format: plz sound (name) \n List of names: boo,clap,babyrage,allah,illuminati,supahot'
+            description: 'Collection of soundboards, format: plz sound (soundname) \n For a list of soundnames, simply type "plz sound" '
         });
     }
 
     async run(message, args) {
-        const boo = './assets/sounds/scare.mp3'
-        const clap = './assets/sounds/clap.mp3'
-        const babyrage = './assets/sounds/babyrage.mp3'
-        const allah = './assets/sounds/allah.mp3'
-        const illuminati = './assets/sounds/illuminati.mp3'
-        const supahot = './assets/sounds/supahot.mp3'
-        var selection = 'placeholder'
+        var sray = ['boo','clap','allah','illuminati','supahot']
+        var selection = 'placeholder' 
+        
+        if (sray.indexOf(args) >= 0) {
+            selection = './assets/sounds/' + args + '.mp3'
 
-        if (args == 'boo') {selection = boo}
-        if (args == 'clap') {selection = clap}
-        if (args == 'babyrage') {selection = babyrage}
-        if (args == 'allah') {selection = allah}
-        if (args == 'illuminati') {selection = illuminati}
-        if (args == 'supahot') {selection = supahot}
-
-
-        if (selection == 'placeholder') {message.reply('please specify the sound  \n Example: "plz sound clap"')}
-        else{
-        const channel = message.member.voiceChannel;
-        if (!channel){
-            message.reply('join a voice channel fam')
-        }
-        else {
-        channel.join()
-        .then(connection => {
-            connection.playFile(selection).on('end', () =>{
-            connection.disconnect();
-                    })
-                })
+            const channel = message.member.voiceChannel;
+            
+            if (!channel){
+                message.reply('join a voice channel fam')
             }
+            else if (!channel.connection) {
+            channel.join()
+            .then(connection => {
+                connection.playFile(selection).on('end', () =>{
+                connection.disconnect();
+                        })
+                    })
+                }
+            else (message.reply('bot already in playing!'))
         }
+        else {message.reply('please specify a proper soundname  \n Example: "plz sound clap" \n list of sound names: ' + sray.join())}
     }
 }
 
