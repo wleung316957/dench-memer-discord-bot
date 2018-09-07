bc62ffd31e40f2e9123e10757dd13788d84fa8f5
wleung316957 <wleung316597x@gmail.com>
2018-09-06 21:27:08 -0700

    Update sound.js

--- commands/soundboard/sound.js
+++ commands/soundboard/sound.js
@@ -12,7 +12,7 @@ class SoundCommand extends commando.Command {
     }
 
     async run(message, args) {
-        var sray = ['boo','clap','allah','supahot','cunt','type','ugly']
+        var sray = ['boo','clap','allah','supahot','cunt','type','ugly','gym','sworn']
         var selection = 'placeholder' 
         
         if (sray.indexOf(args) >= 0) {
